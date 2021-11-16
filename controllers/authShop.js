b25e046706d690e92fd8f655ef64d5cf789f631d
Navid <navid.naseri.079@gmail.com>
2021-11-16 12:40:22 +0330

    Added Email already exists validation(Not handled)

--- controllers/authShop.js
+++ controllers/authShop.js
@@ -1,7 +1,36 @@
-const User = require('../models/user');
+const { validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 exports.postShopSignup = (req, res, next) => {
   const data = req.body.data;
+
+  const errors = validationResult(req);
+  let statusCode;
+  let messages = [];
+  let values = {};
+  let conflicts = [];
+  if (!errors.isEmpty()) {
+    for (let error of errors.array()) {
+      const [fetchedStatusCode, fetchedMessage, fetchedConflict] = error.msg.split('~');
+      if (statusCode && statusCode != fetchedStatusCode) {
+        break;
+      }
+      statusCode = fetchedStatusCode;
+
+      messages.push(fetchedMessage);
+      conflicts.push(fetchedConflict);
+      values[fetchedConflict] = data[fetchedConflict];
+    }
+
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.messages = messages;
+    error.values = values;
+    error.conflicts = conflicts;
+
+    throw error;
+  }
 
   const user = new User({ ...data });
   user
--- routes/authShop.js
+++ routes/authShop.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const { body } = require('express-validator');
+
+const User = require('../models/User');
+
 const authShopController = require('../controllers/authShop');
 
 const router = express.Router();
 
-router.post('/signup', authShopController.postShopSignup);
+router.post(
+  '/signup',
+  [body('data').custom(data => {
+    // Email Already Registered
+    if (!data.email) return true;
+    return User.findOne({ email: data.email }).then(user => {
+      if (user) {
+        return Promise.reject('409~Email already registered~email');
+      }
+    });
+    return true;
+  })],
+  authShopController.postShopSignup
+);
 
 module.exports = router;
