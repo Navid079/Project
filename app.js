cfda7e38dc80483897df1aa5c2524a85c89c8ff6
Navid <navid.naseri.079@gmail.com>
2021-11-16 12:40:43 +0330

    Added error handler middleware

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ app.use(express.json());
 
 app.use('/shop', authShopRouter);
 
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  let status = err.statusCode;
+  if (!status) {
+    status = 500;
+  }
+  const response = { message: 'An error occured', data: {} };
+  if (err.messages) response.data.messages = err.messages;
+  if (err.values) response.data.values = err.values;
+  if (err.conflicts) response.data.conflicts = err.conflicts;
+
+  res.status(status).json(response);
+});
+
 mongoose
   .connect('mongodb://localhost:27017/theProject')
   .then(result => {
