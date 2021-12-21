2bc6a880a52c6d56b6a250d17c76534c4f987c93
amin75t <aminshahini18@gmail.com>
2021-12-21 16:17:28 +0330

    (mobile) add Structure(html and some css)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import ShopIndex from './pages/ShopIndex';
+import EnterPage from './pages/Login-Sinup/EnterPage';
 
 import './App.css';
 
 function App() {
   return (
-    <div className='container'>
-      <ShopIndex />
+    <div className='main'>
+      <EnterPage />
     </div>
   );
 }
