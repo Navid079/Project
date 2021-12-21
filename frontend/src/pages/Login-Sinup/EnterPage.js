af85c57b03d877cfb39bdeb2d94881f62ba467b4
Hani Jahan <haniahn1115@gmail.com>
2021-12-21 21:43:44 +0330

    sign up textInput added

--- frontend/src/pages/Login-Sinup/EnterPage.js
+++ frontend/src/pages/Login-Sinup/EnterPage.js
@@ -1,5 +1,9 @@
 import React from "react";
+
+import TextInput from "../../components/UI/TextInput/TextInput";
+
 import "./EnterPage.css";
+
 export default function EnterPage() {
   return (
     <>
@@ -10,6 +14,38 @@ export default function EnterPage() {
         <div className="input-forms__signup-form">
           <div className="input-forms__signup-forms__inputs">
             {/* This is for input componentss  */}
+            <TextInput
+              className="input-forms__signup-forms__username"
+              type="text"
+              placeholder="نام کاربری"
+            />
+            <TextInput
+              className="input-forms__signup-forms__tel"
+              type="tel"
+              placeholder="تلفن همراه"
+            />
+            <TextInput
+              className="input-forms__signup-forms__email"
+              type="email"
+              placeholder="ایمیل"
+            />
+            <TextInput
+              className="input-forms__signup-forms__password"
+              type="password"
+              placeholder="گذرواژه"
+            />
+            <TextInput
+              className="input-forms__signup-forms__confirm-password"
+              type="password"
+              placeholder="تایید گذرواژه"
+            />
+            <a href="#">حساب کاربری دارید؟</a>
+            <button className="">ثبت</button>
+            {/* component should change later */}
+            {/* <TextInput type="submit" value="ثبت" /> */}
+
+            <span className="signup-forms__warnig-region--error-required-field"></span>
+
             <input type="hidden" value={"temp"} />
             {/* for backend */}
           </div>
