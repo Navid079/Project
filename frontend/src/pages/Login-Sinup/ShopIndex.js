156d4afb0baf863643f8bbacafd3875976217033
Hani Jahan <haniahn1115@gmail.com>
2021-12-22 13:25:03 +0330

    ShopIndex.js commented and toggle refactored

--- frontend/src/pages/Login-Sinup/ShopIndex.js
+++ frontend/src/pages/Login-Sinup/ShopIndex.js
@@ -6,70 +6,75 @@ import Button from "../../components/UI/Button/Button";
 import Toggle from "../../components/UI/Toggle";
 
 export default function ShopIndex() {
-  
   const toggleHandler = (position) => {
     // will be added
     // position argument can be "left" or "right"
-  }
-  
+  };
+
   return (
     <div className="shop-index">
+      {/* =========         WAVE         ========= */}
       <div className="shop-index__wave--mobile" />
+
+      {/* =========        TOGGLE        ========= */}
       <Toggle
         className="shop-index__toggle"
         leftLabel="ورود"
         rightLabel="ثبت نام"
         onToggle={toggleHandler}
       />
-      <div className="input-forms">
-        <div className="input-forms__login-form"></div>
-        <div className="input-forms__signup-form">
-          <form className="input-forms__signup-forms__inputs">
-            {/* This is for input componentss  */}
-            <IconInput
-              icon="healthicons:ui-user-profile-outline"
-              flipped={true}
-              className="input-forms__signup-forms__username"
-              type="text"
-              placeholder="نام کاربری"
-            />
-            <IconInput
-              icon="akar-icons:phone"
-              flipped={true}
-              className="input-forms__signup-forms__tel"
-              type="tel"
-              placeholder="تلفن همراه"
-            />
-            <IconInput
-              icon="mdi-light:email"
-              flipped={true}
-              className="input-forms__signup-forms__email"
-              type="email"
-              placeholder="ایمیل"
-            />
-            <div className="input-forms__signup-forms__password-container">
-              <IconInput
-                icon="carbon:password"
-                flipped={true}
-                className="input-forms__signup-forms__password"
-                type="password"
-                placeholder="گذرواژه"
-              />
-              <IconInput
-                className="input-forms__signup-forms__confirm-password"
-                type="password"
-                placeholder="تایید گذرواژه"
-              />
-            </div>
-            <a href="#">حساب کاربری دارید؟</a>
-            <Button className="">ثبت</Button>
 
-            <span className="signup-forms__warnig-region--error-required-field"></span>
+      {/* =========    FORM CONTAINERS    ========= */}
+      <div className="shop-index__controls">
+        {/* ========= LOGIN FORM CONTAINER ========= */}
+        <form className="shop-index__login-controls"></form>
 
-            <input type="hidden" value={"temp"} />
-            {/* for backend */}
-          </form>
-        </div>
+        {/* ========= SIGNUP FORM CONTAINER ========= */}
+        <form className="shop-index__signup-controls">
+          <IconInput
+            icon="healthicons:ui-user-profile-outline"
+            flipped={true}
+            className="input-forms__signup-forms__username"
+            type="text"
+            placeholder="نام کاربری"
+          />
+          <IconInput
+            icon="akar-icons:phone"
+            flipped={true}
+            className="input-forms__signup-forms__tel"
+            type="tel"
+            placeholder="تلفن همراه"
+          />
+          <IconInput
+            icon="mdi-light:email"
+            flipped={true}
+            className="input-forms__signup-forms__email"
+            type="email"
+            placeholder="ایمیل"
+          />
+          <div className="input-forms__signup-forms__password-container">
+            <IconInput
+              icon="carbon:password"
+              flipped={true}
+              className="input-forms__signup-forms__password"
+              type="password"
+              placeholder="گذرواژه"
+            />
+            <IconInput
+              className="input-forms__signup-forms__confirm-password"
+              type="password"
+              placeholder="تایید گذرواژه"
+            />
+          </div>
+          <a href="#">حساب کاربری دارید؟</a>
+          <Button className="">ثبت</Button>
+
+          <span className="signup-forms__warnig-region--error-required-field"></span>
+
+          <input type="hidden" value={"temp"} />
+          {/* for backend */}
+        </form>
+
         <div className="signup-forms__warnig-region">
           <a href="#">حساب کاربری ندارید؟</a>
           <span className="signup-forms__warnig-region--errorUsername"></span>
