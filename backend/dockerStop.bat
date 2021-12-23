73d31d1cdb5efb7cc3b2acd7a178fbf8b4d54ee3
Navid <navid.naseri.079@gmail.com>
2021-12-24 01:12:45 +0330

    minor bug fixed

--- backend/dockerStop.bat
+++ backend/dockerStop.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo "Stopping backend"
-docker rm --force proj > /dev/null 2>&1
+docker rm --force proj
 echo "Backend stoped"
 
 echo "Stopping mongodb"
-docker rm --force mongo > /dev/null 2>&1
+docker rm --force mongo
 echo "Mongodb stopped"
 
 echo "Done!"
