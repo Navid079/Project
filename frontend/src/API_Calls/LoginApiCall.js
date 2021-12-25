82db49603de66474beb3d3c4e0dde178ecc02486
Nemati-MAmin <amin.nz399@gmail.com>
2021-12-26 01:27:16 +0330

    refactor

--- frontend/src/API_Calls/LoginApiCall.js
+++ frontend/src/API_Calls/LoginApiCall.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const loginApiCall = async (user, reducer) => {
   try {
     const res = await axios.post('http://localhost:3005/shop/login', user);
-    console.log(res.data.data)
     reducer({ type: 'SET', data: res.data.data});
   } catch (error) {}
 };
