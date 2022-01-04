6fc7cfa325be01d330162f6674ec0463ecb3fd24
Hani Jahan <haniahn1115@gmail.com>
2022-01-04 12:40:48 +0330

    hoverHandler added

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import DashboardTab from './../../components/Dashboard/DashboardTab';
 
@@ -6,17 +6,23 @@ import "./Dashboard.css";
 
 export default function Dashboard() {
 
+  const [appendTab, setAppendTab] = useState(false); 
+
   const hoverHandler = (event) => {
-    
+    setAppendTab(true)
+  }
+
+  const exitHandler = (event) => {
+    setAppendTab(false)
   }
 
   return (
-    <div className="dashboard">
+    <div className="dashboard" onMouseEnter={hoverHandler} onMouseLeave={exitHandler}>
     {/* on hover, showText changes to true */}
-      <DashboardTab showText={true} icon="tabler:report-analytics" className="test">داشبورد</DashboardTab>
-      <DashboardTab icon="tabler:report-analytics" className="test">گزارشات</DashboardTab>
-      <DashboardTab icon="ion:newspaper" className="test">خبرنامه</DashboardTab>
-      <DashboardTab icon="gridicons:product" className="test">محصولات</DashboardTab>
+      <DashboardTab showText={appendTab} icon="tabler:report-analytics" className="test">داشبورد</DashboardTab>
+      <DashboardTab showText={appendTab} icon="tabler:report-analytics" className="test">گزارشات</DashboardTab>
+      <DashboardTab showText={appendTab} icon="ion:newspaper" className="test">خبرنامه</DashboardTab>
+      <DashboardTab showText={appendTab} icon="gridicons:product" className="test">محصولات</DashboardTab>
     </div>
   );
 }
