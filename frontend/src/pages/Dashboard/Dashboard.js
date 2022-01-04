bdc17c759499892db8530cf8912406856a55f15f
Hani Jahan <haniahn1115@gmail.com>
2022-01-04 12:44:54 +0330

    code formated

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,28 +1,50 @@
 import React, { useState } from "react";
 
-import DashboardTab from './../../components/Dashboard/DashboardTab';
+import DashboardTab from "./../../components/Dashboard/DashboardTab";
 
 import "./Dashboard.css";
 
 export default function Dashboard() {
-
-  const [appendTab, setAppendTab] = useState(false); 
+  const [appendTab, setAppendTab] = useState(false);
 
   const hoverHandler = (event) => {
-    setAppendTab(true)
-  }
+    setAppendTab(true);
+  };
 
   const exitHandler = (event) => {
-    setAppendTab(false)
-  }
+    setAppendTab(false);
+  };
 
   return (
-    <div className="dashboard" onMouseEnter={hoverHandler} onMouseLeave={exitHandler}>
-    {/* on hover, showText changes to true */}
-      <DashboardTab showText={appendTab} icon="tabler:report-analytics" className="test">داشبورد</DashboardTab>
-      <DashboardTab showText={appendTab} icon="tabler:report-analytics" className="test">گزارشات</DashboardTab>
-      <DashboardTab showText={appendTab} icon="ion:newspaper" className="test">خبرنامه</DashboardTab>
-      <DashboardTab showText={appendTab} icon="gridicons:product" className="test">محصولات</DashboardTab>
+    <div
+      className="dashboard"
+      onMouseEnter={hoverHandler}
+      onMouseLeave={exitHandler}
+    >
+      <DashboardTab
+        showText={appendTab}
+        icon="tabler:report-analytics"
+        className="test"
+      >
+        داشبورد
+      </DashboardTab>
+      <DashboardTab
+        showText={appendTab}
+        icon="tabler:report-analytics"
+        className="test"
+      >
+        گزارشات
+      </DashboardTab>
+      <DashboardTab showText={appendTab} icon="ion:newspaper" className="test">
+        خبرنامه
+      </DashboardTab>
+      <DashboardTab
+        showText={appendTab}
+        icon="gridicons:product"
+        className="test"
+      >
+        محصولات
+      </DashboardTab>
     </div>
   );
 }
