5938bd608d7ed0ed6bde8fc19811c58698dfaef7
Navid <navid.naseri.079@gmail.com>
2022-01-04 15:24:17 +0330

    changed validation structure

--- backend/routes/shop/auth.js
+++ backend/routes/shop/auth.js
@@ -9,6 +9,7 @@ const User = require('../../models/User');
 const shopAuthController = require('../../controllers/shop/auth');
 const validation = require('../../middlewares/validation');
 const authentication = require('../../middlewares/authentication');
+const validators = require('../../validators/shop/auth');
 
 // Utility functions
 const isPhoneNumber = require('../../utils/isPhoneNumber');
@@ -21,60 +22,8 @@ const router = express.Router();
 // This endpoint is used for signing up of sellers
 router.post(
   '/signup',
-  // ========== Validation ========== //
-  [
-    body('data')
-      .custom(data => {
-        //Data is not correct
-        if (!data.username) throw new Error('422~Data is not correct~username');
-        if (!data.devId) throw new Error('422~Data is not correct~devId');
-        if (!data.phone) throw new Error('422~Data is not correct~phone');
-        if (!data.password) throw new Error('422~Data is not correct~password');
-        if (!data.confirm) throw new Error('422~Data is not correct~confirm');
-        return true;
-      })
-      .custom(data => {
-        // Email Already Registered
-        if (!data.email) return true;
-        return User.findOne({ email: data.email }).then(user => {
-          if (user) {
-            return Promise.reject('409~Email already registered~email');
-          }
-        });
-      })
-      .custom(data => {
-        //Phone Number Already Registered
-        return User.findOne({ phone: data.phone }).then(user => {
-          if (user) {
-            return Promise.reject('409~Phone number already registered~phone');
-          }
-        });
-      })
-      .custom(data => {
-        //Not a phone number
-        if (!isPhoneNumber(data.phone))
-          throw new Error('422~Not a phone number~phone');
-        return true;
-      })
-      .custom(data => {
-        //Not an email address
-        if (!isEmail(data.email))
-          throw new Error('422~Not an email address~email');
-        return true;
-      })
-      .custom(data => {
-        if (!isStrongPassword(data.password))
-          throw new Error('422~Password is weak~password');
-        return true;
-      })
-      .custom(data => {
-        if (data.password !== data.confirm)
-          throw new Error("422~Passwords don't match~confirm");
-        return true;
-      }),
-    validation,
-  ],
-  // ========== End of Validation ========== //
+  validators.signup,
+  validation,
   shopAuthController.postShopSignup
 );
 
@@ -82,63 +31,17 @@ router.post(
 // This endpoint is used for logging in of sellers
 router.post(
   '/login',
-  [
-    // ========== Validation ========== //
-    body('data')
-      .custom(data => {
-        //Data is not correct
-        if (!data.devId) throw new Error('422~Data is not correct~devId');
-        if (!data.user) throw new Error('422~Data is not correct~user');
-        if (!data.password) throw new Error('422~Data is not correct~password');
-        if (!data.userType) throw new Error('422~Data is not correct~userType');
-        return true;
-      })
-      .custom(data => {
-        //Password invalid (due to weakness)
-        if (!isStrongPassword(data.password))
-          throw new Error('401~Wrong password~password');
-        return true;
-      })
-      .custom(data => {
-        //Not a username Type
-        if (data.userType != 'email' && data.userType != 'phone' && data.userType != 'username')
-          throw new Error('422~Unknown username type~userType');
-        return true;
-      })
-      .custom(data => {
-        //Not a phone number
-        if (data.userType === 'phone' && !isPhoneNumber(data.user))
-          throw new Error('422~Not a phone number~user');
-        return true;
-      })
-      .custom(data => {
-        //Not an email address
-        if (data.userType === 'email' && !isEmail(data.user))
-          throw new Error('422~Not an email address~user');
-        return true;
-      }),
-    validation,
-  ],
-  // ========== End of Validation ========== //
+  validators.login,
+  validation,
   shopAuthController.postShopLogin
 );
 
 router.post(
   '/refresh',
-  [
-    // ========== Validation ========== //
-    body('data').custom(data => {
-      //Data is not correct
-      if (!data.devId) throw new Error('422~Data is not correct~devId');
-      if (!data.token) throw new Error('422~Data is not correct~token');
-      if (!data.refresh) throw new Error('422~Data is not correct~refresh');
-      return true;
-    }),
-    validation,
-  ],
-  // ========== End of Validation ========== //
-  authentication.refreshCompiler,
+  validators.refresh,
+  validation,
   authentication.tokenCompiler,
+  authentication.refreshCompiler,
   shopAuthController.postShopRefresh
 );
 
