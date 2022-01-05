c1b69a4e0d10d4c0509c0a55a600b8f4a11937a7
Hani Jahan <haniahn1115@gmail.com>
2022-01-05 18:33:27 +0330

    Navbar added

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 
-import './Dashboard.css';
+import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+
+import './Dashboard.css';
 
 export default function Dashboard() {
   return (
     <div className='dashboard'>
-      <Sidebar />
+      <Navbar />
+      <div>
+        <Sidebar />
+        <div className='dashboard__container'></div>
+      </div>
     </div>
   );
 }
