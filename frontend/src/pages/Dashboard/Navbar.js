cad5d3d356910da848f15ebeddab62c7a9294cb4
Hani Jahan <haniahn1115@gmail.com>
2022-01-09 14:22:13 +0330

    navbar fixed

--- frontend/src/pages/Dashboard/Navbar.js
+++ frontend/src/pages/Dashboard/Navbar.js
@@ -6,27 +6,28 @@ import TextInput from './../../components/UI/TextInput/TextInput';
 export default function Navbar(props) {
   return (
     <div className='navbar'>
-      <div className='navbar__radius'>
-        {/* back button */}
-        <div className='navbar__back'>
-          <Icon icon='eva:arrow-ios-back-outline' />
-        </div>
+      {/* search component */}
+      <Icon icon='fluent:search-24-regular' />
+      <TextInput
+        className='navbar__search-bar'
+        type='search'
+        placeholder='جستجو'
+      ></TextInput>
+
+      {/* website logo */}
+      <div className='dashboard__logo'></div>
+
+      {/* logout button */}
+      <div className='navbar__logout'>
+        <Icon icon='bx:bx-log-out' />
       </div>
-      <div className='navbar__container'>
-        {/* search component */}
-        <TextInput
-          className='navbar__search-bar'
-          type='search'
-          placeholder=' جستجو'
-        ></TextInput>
-        {/* website logo */}
-        <div className='dashboard__logo'></div>
-        {/* logout button */}
-        <div className='navbar__logout'>
-          <Icon icon='bx:bx-log-out' />
-        </div>
-        {/* profile photo */}
-        <div className='navbar__profile'></div>
+
+      {/* profile photo */}
+      <div className='navbar__profile'></div>
+
+      {/* back button */}
+      <div className='navbar__back'>
+        <Icon icon='eva:arrow-ios-back-outline' />
       </div>
     </div>
   );
