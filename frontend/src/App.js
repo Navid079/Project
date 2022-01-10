531b5b27ee2f5b47c61e5410fb298913185aa81e
Navid <navid.naseri.079@gmail.com>
2022-01-10 17:07:12 +0330

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Index from './pages/Index/Index';
-import Dashboard from "./pages/Dashboard/Dashboard";
+import Dashboard from './pages/Dashboard/Dashboard';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <Index />
-      {/* <Dashboard /> */}
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <Routes>
+          <Route exact path='/' element={<Index />}></Route>
+          <Route exact path='/dashboard' element={<Dashboard />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
