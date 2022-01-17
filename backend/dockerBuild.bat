752e83bd336a82945c0ab36af2bfc44c8514fe49
Navid <navid.naseri.079@gmail.com>
2022-01-17 10:10:48 +0330

    dockerBuild edited

--- backend/dockerBuild.bat
+++ backend/dockerBuild.bat
@@ -1,5 +1,6 @@
 @echo off
 
 echo "Building/Updating backend image"
+docker rmi theproj:dev > /dev/null 2>&1
 docker build --tag theproj:dev .
 echo "Backend image is ready"
--- backend/dockerBuild.sh
+++ backend/dockerBuild.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo "Building/Updating backend image"
+docker rmi theproj:dev > /dev/null 2>&1
 docker build --tag theproj:dev . > /dev/null 2>&1
 echo "Backend image is ready"
