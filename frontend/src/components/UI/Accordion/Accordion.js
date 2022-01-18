36ab514845fe16a904549e78b2c4a3f5ab8dce41
Navid <navid.naseri.079@gmail.com>
2022-01-18 23:21:24 +0330

    added inactive property to accordion to control title color

--- frontend/src/components/UI/Accordion/Accordion.js
+++ frontend/src/components/UI/Accordion/Accordion.js
@@ -2,20 +2,26 @@ import React, { useRef } from 'react';
 
 import './Accordion.css';
 
-const Accordion = ({ className, title, children }) => {
-
+const Accordion = ({ className, title, children, inactive }) => {
   const body = useRef();
 
-  const toggleHandler = (event) => {
+  const toggleHandler = event => {
     body.current.classList.toggle('accordion__body--show');
-  }
+  };
 
   return (
     <div className={`accordion ${className}`}>
-      <div className='accordion__title' onClick={toggleHandler}>
+      <div
+        className={`accordion__title ${
+          inactive ? 'accordion__title--inactive' : ''
+        }`}
+        onClick={toggleHandler}
+      >
         {title}
       </div>
-      <div className='accordion__body' ref={body}>{children}</div>
+      <div className='accordion__body' ref={body}>
+        {children}
+      </div>
     </div>
   );
 };
