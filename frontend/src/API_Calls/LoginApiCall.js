c6470680d92a48aec430e7aafbe54fc947e1fa30
Navid <navid.naseri.079@gmail.com>
2022-01-22 16:09:58 +0330

    changed reducer to dispatch in api calls

--- frontend/src/API_Calls/LoginApiCall.js
+++ frontend/src/API_Calls/LoginApiCall.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 const api = process.env.REACT_APP_API_URL;
 
-const LoginApiCall = async (user, reducer) => {
+const LoginApiCall = async (user, dispatch) => {
   try {
     const res = await axios.post(`${api}/shop/login`, user);
-    reducer({ type: 'SET_USER', data: res.data.data });
-    reducer({ type: 'LOGIN' });
+    dispatch({ type: 'SET_USER', data: res.data.data });
+    dispatch({ type: 'LOGIN' });
     return true;
   } catch (error) {
     error.response.data = error.response.data.data;
     error.response.page = 'login';
-    reducer({ type: 'SET_ERROR', data: error.response });
+    dispatch({ type: 'SET_ERROR', data: error.response });
     return false;
   }
 };
--- frontend/src/API_Calls/SignupApiCall.js
+++ frontend/src/API_Calls/SignupApiCall.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 const api = process.env.REACT_APP_API_URL;
 
-const SignupApiCall = async (user, reducer) => {
+const SignupApiCall = async (user, dispatch) => {
   try {
     const res = await axios.post(`${api}/shop/signup`, user);
-    reducer({ type: 'SET_USER', data: res.data.data });
-    reducer({ type: 'LOGIN' });
+    dispatch({ type: 'SET_USER', data: res.data.data });
+    dispatch({ type: 'LOGIN' });
     return true;
   } catch (error) {
     error.response.data = error.response.data.data;
     error.response.page = 'signup';
-    reducer({ type: 'SET_ERROR', data: error.response });
+    dispatch({ type: 'SET_ERROR', data: error.response });
     return false;
   }
 };
