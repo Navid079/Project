e54f1d1afeed5f862b11fa84960e9206635c2d26
Navid <navid.naseri.079@gmail.com>
2022-01-23 19:02:11 +0330

    Ordered List Component added

---
+++ frontend/src/components/UI/OrderedList/OrderedList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './OrderedList.css';
+
+const OrderedList = ({ className, children }) => {
+  const items = children.map(({ progress, item }, index) => (
+    <li>
+      <ListBullet number={index + 1} progress={progress} />
+      {item}
+    </li>
+  ));
+
+  return <ol className={`ordered-list ${children}`}>{items}</ol>;
+};
+
+export default OrderedList;
