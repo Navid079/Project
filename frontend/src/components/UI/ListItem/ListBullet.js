1731a61f81e4ff217db3e42aa3439bba4b18ff3c
Hani Jahan <haniahn1115@gmail.com>
2022-01-23 12:44:09 +0330

    bullet animation added

--- frontend/src/components/UI/ListItem/ListBullet.js
+++ frontend/src/components/UI/ListItem/ListBullet.js
@@ -3,10 +3,10 @@ import './ListBullet.css';
 
 export default function ListBullet({ className, number, progress }) {
   const lines = 8 - Math.floor(progress / 14.28);
-  const dashArraySum = Math.round(Math.PI / lines * 100);
+  const dashArraySum = Math.round((Math.PI / lines) * 100);
   const linePercentage =
-  Math.round((0.5 + Math.ceil(progress / 2) / 100) * dashArraySum * 100) /
-  100;
+    Math.round((0.5 + Math.ceil(progress / 2) / 100) * dashArraySum * 100) /
+    100;
   const spacePercentage = dashArraySum - linePercentage;
 
   const bg = `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='%236AD727FF' stroke-width='5' stroke-dasharray='${Math.floor(
@@ -18,11 +18,12 @@ export default function ListBullet({ className, number, progress }) {
   console.log(bg);
 
   return (
-    <div
-      className={`progress-bullet ${className}`}
-      style={{ backgroundImage: bg }}
-    >
-      {number}
+    <div className={`progress-bullet ${className}`}>
+      <div
+        className='progress-bullet__bg'
+        style={{ backgroundImage: bg }}
+      ></div>
+      <div className='progress-bullet__content'>{number}</div>
     </div>
   );
 }
