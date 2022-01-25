ec448ee60f3841c01bf94e75d61d4665df613c00
Navid <navid.naseri.079@gmail.com>
2022-01-25 21:19:11 +0330

    controller bodies added

---
+++ backend/controllers/shop/profile.js
@@ -0,0 +1,19 @@
+exports.patchShopProfile = async (req, res, next) => {
+  // TODO: complete and test this endpoint
+};
+
+exports.getShopProfile = (req, res, next) => {
+  // TODO: complete and test this endpoint
+};
+
+exports.postShopProfileMediaParam = (req, res, next) => {
+  // TODO: complete and test this endpoint
+};
+
+exports.getShopProfileAvatar = (req, res, next) => {
+  // TODO: complete and test this endpoint
+};
+
+exports.getShopProfileMedia = (req, res, next) => {
+  // TODO: complete and test this endpoint
+};
--- backend/routes/shop/profile.js
+++ backend/routes/shop/profile.js
@@ -2,33 +2,28 @@
 const express = require('express');
 
 // Controller and middlewares
-
+const controller = require('../../controllers/shop/profile');
 
 const router = express.Router();
 
-// TODO: write a controller for this endpoint
-// POST /shop/profile/<profile attribute>
+// PATCH /shop/profile/
 // This endpoint is used for setting user's non-media profile attributes
-router.post('/:attribute', /* controller */)
+router.patch('/', controller.patchShopProfile);
 
-// TODO: write a controller for this endpoint
 // GET /shop/profile/
 // This endpoint is used for getting full user's non-media profile attributes
-router.get('/', /*controller */)
+router.get('/', controller.getShopProfile);
 
-// TODO: write a controller for this endpoint 
 // POST /shop/profile/media/<profile attribute>
 // This endpoint is used for uploading user's profile media attributes
-router.post('/media/:attribute', /* controller */)
+router.post('/media/:attribute', controller.postShopProfileMediaParam);
 
-// TODO: write a controller for this endpoint
 // GET /shop/profile/avatar
 // This endpoint is used for getting user's avatar
-router.get('/avatar', /* controller */)
+router.get('/avatar', controller.getShopProfileAvatar);
 
-// TODO: write a controller for this endpoint
 // GET /shop/profile/media
-// This endpoint is used for getting all user's media documents 
-router.get('/media', /* controller */)
+// This endpoint is used for getting all user's media documents
+router.get('/media', controller.getShopProfileMedia);
 
 module.exports = router;
