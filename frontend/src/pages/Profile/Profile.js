ff1a358aa9e5d54dae3acd1e18b6d0bc2d1ed7b6
Navid <navid.naseri.079@gmail.com>
2022-01-27 13:51:09 +0330

    profile page added (Not responsive)

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -3,10 +3,13 @@ import { Icon } from '@iconify/react';
 import Accordion from './../../components/UI/Accordion/Accordion';
 import Toggle from './../../components/UI/Toggle';
 import TextInput from './../../components/UI/TextInput/TextInput';
+import OrderedList from '../../components/UI/OrderedList/OrderedList'
 
 import './Profile.css';
+import UploadButton from '../../components/Profile/UploadButton';
 
 export default function Profile() {
+
   const personalSection = (
     <Accordion title='تکمیل اطلاعات شخصی' inactive>
       <div className='profile__accordion-body'>
@@ -40,9 +43,23 @@ export default function Profile() {
     </Accordion>
   );
 
-  const uploadSection = <Accordion title='آپلود اطلاعات' inactive>
-    <div className='profile__accordion-body'></div>
-  </Accordion>;
+  const uploadSection = (
+    <Accordion title='آپلود اطلاعات' inactive>
+      <div className='profile__accordion-body'>
+        <UploadButton>آپلود مدارک</UploadButton>
+      </div>
+    </Accordion>
+  );
 
-  return <div style={{ width: '100%' }}>{personalSection}</div>;
+  const visualSection = <Accordion title='اطلاعات نمایشی' inactive></Accordion>;
+
+  const listItems = [
+    {item: personalSection, progress: 0},
+    {item: uploadSection, progress: 0},
+    {item: visualSection, progress: 0},
+  ]
+
+  const list = <OrderedList>{listItems}</OrderedList>
+
+  return <div className='profile'>{list}</div>;
 }
