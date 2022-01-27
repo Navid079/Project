54b700225486a7959942d0fb0928681cd8a214a1
Navid <navid.naseri.079@gmail.com>
2022-01-27 03:31:08 +0330

    post-shop-profile-media-param controller added

--- backend/controllers/shop/profile.js
+++ backend/controllers/shop/profile.js
@@ -10,15 +10,17 @@ exports.patchShopProfile = async (req, res, next) => {
   const response = {
     message: 'User profile updated successfully',
     data: {
-      name: {
-        first: user.name.first,
-        last: user.name.last,
+      user: {
+        name: {
+          first: user.name.first,
+          last: user.name.last,
+        },
+        shopAddress: user.shopAddress,
+        postalCode: user.postalCode,
+        nationalCode: user.nationalCode,
+        idNumber: user.idNumber,
+        validated: user.validated,
       },
-      shopAddress: user.shopAddress,
-      postalCode: user.postalCode,
-      nationalCode: user.nationalCode,
-      idNumber: user.idNumber,
-      validated: user.validated,
     },
   };
 
@@ -31,15 +33,17 @@ exports.getShopProfile = (req, res, next) => {
   const response = {
     message: 'User profile fetched',
     data: {
-      name: {
-        first: user.name.first,
-        last: user.name.last,
+      user: {
+        name: {
+          first: user.name.first,
+          last: user.name.last,
+        },
+        shopAddress: user.shopAddress,
+        postalCode: user.postalCode,
+        nationalCode: user.nationalCode,
+        idNumber: user.idNumber,
+        validated: user.validated,
       },
-      shopAddress: user.shopAddress,
-      postalCode: user.postalCode,
-      nationalCode: user.nationalCode,
-      idNumber: user.idNumber,
-      validated: user.validated,
     },
   };
 
@@ -47,6 +51,17 @@ exports.getShopProfile = (req, res, next) => {
 };
 
 exports.postShopProfileMediaParam = (req, res, next) => {
+  const user = req.compiled.user;
+  user.mediaLink.push(req.file.filename);
+  await user.save();
+
+  res.status(201).json({
+    message: 'Media successfully uploaded',
+    data: {},
+  });
+};
+
+exports.postShopProfileAvatar = (req, res, next) => {
   // TODO: complete and test this endpoint
 };
 
