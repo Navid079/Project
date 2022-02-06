669899cdf8338efcef9ddabf347abfdb504de452
Hani Jahan <haniahn1115@gmail.com>
2022-02-06 17:38:59 +0330

    added popup

--- frontend/src/components/UI/Popup/Popup.js
+++ frontend/src/components/UI/Popup/Popup.js
@@ -1,6 +1,12 @@
+import { Icon } from '@iconify/react';
 import React from 'react';
 import './Popup.css';
 
-export default function Popup({ className, children }) {
-  return <div dir="rtl" className={`popup ${className}`}>{children}</div>;
+export default function Popup({ className, icon, children }) {
+  return (
+    <div className={`popup ${className}`}>
+      <Icon className='popup__icon' icon={icon}></Icon>
+      {children}
+    </div>
+  );
 }
